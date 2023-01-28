fe268bb6c6333c3719c030310c19f0ebeb63c8a0
daniellaas29 <123763126+daniellaas29@users.noreply.github.com>
2023-01-28 20:11:28 +0700

    Add files via upload

---
+++ homework week4-1.js
@@ -0,0 +1,111 @@
+// Buatlah 100 nilai random (1 sampai 50] pada 1 array
+let index = 1
+while (index <=100){
+    index++;
+    console.log(Math.ceil(Math.random() * 50)+ 1)
+}
+return RandomNumbers
+console.log(RandomNumbers)
+
+//Pecahlah menjadi 2 array berdasarkan indexnya, yakni array pada index genap ddan array pada index ganjil, gunakan method push() untuk menambahkan nilai baru pada array
+for (let index = 0; index <100; index++){
+    if (index % 2 == 0){
+        return odd
+    } else {
+       return even
+    }
+    console.log(index)
+}
+
+//Gunakan kedua buah array untuk mendapatkan min, max, total, rata-rata
+
+//MIN
+//even numbers
+function findMinimum(even){
+    for (let even = 0 ;index <100; index++){
+        console.log(even)
+    }
+}
+let result = findMinimun (even)
+//odd numbers
+function findMinimum(odd){
+    for (let odd = 0 ;index <100; index++){
+        console.log(odd)
+    }
+}
+result findMinimun(odd)
+
+//MAX
+//even numbers
+function findMaximum(even){
+    for (let even = 0 ;index <100; index++){
+        console.log(even)
+    }
+    result 50
+}
+//odd numbers
+let result = findMaximum (even)
+
+function findMaximum(odd){
+    for (let odd = 0 ;index <100; index++){
+        console.log(odd)
+    }
+    result 49
+}
+let result = findMaximum (odd)
+
+//TOTAL
+//even numbers
+let EvenNumbers = [even]
+function sumArray(even){
+    const ourArray = [even];
+    let sum = 0;
+    for (let even = 0; even < ourArray, length; even+=1){
+        SumEvenNumbers += array[even]
+    }
+    console.log(SumEvenNumbers)
+    return SumEvenNumbers
+}
+//odd numbers
+let OddNumbers = [odd]
+function sumArray(odd){
+    const ourArray = [odd];
+    let sum = 0;
+    for (let odd = 0; even < ourArray, length; even+=1){
+        SumOddNumbers += array[odd]
+    }
+    console.log(SumOddNumbers)
+    return SumOddNumbers
+}
+
+//RATA-RATA
+//even numbers
+
+function FindModulusEven{
+    for(let even )
+}
+console.log(ModulusEven)
+return ModulusEven
+//odd numbers
+let OddNumbers = [odd]
+function sumArray(odd){
+    const ourArray = [odd];
+    let sum = 0;
+    for (let odd = 0; even < ourArray, length; even+=1){
+        SumOddNumbers += array[odd]
+    }
+    console.log(SumOddNumbers)
+    return SumOddNumbers
+}
+
+//Bandingkan kedua buah array
+function CompareTotal(SumOddNumbers,sumEvenNumbers){
+
+    if (SumEvenNumbers < SumOddNumbers){
+        return "Total lebih besar dari bilangan ganjil";
+    } else if (SumEvenNumbers > SumOddNumbers){
+        return "Total lebih besar dari bilangan genap";
+    }else{
+        return "Total bilangan ganjil setara dengan total bilangan genap"
+    }
+    }
